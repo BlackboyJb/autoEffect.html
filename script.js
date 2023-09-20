d948863d044c1446a171f8c8dc92677a9d603592
Adegoke Odujoko <136864558+BlackboyJb@users.noreply.github.com>
2023-09-20 02:35:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const textEL = document.getElementById('text')
+const speedEL = document.getElementById('speed')
+
+const text = prompt('Type your Text')
+
+let idx = 1
+let speed = 300 / speedEL.value;
+
+writeText()
+
+
+function writeText() {
+    textEL.innerHTML = text.slice(0, idx)
+
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+    setTimeout(writeText, speed)
+}
+
+speedEL.addEventListener('input', (e) => speed = 300 / e.target.value)
